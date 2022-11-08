289360ec16045f473930580b0c86e2c946297a59
MrGozXD <yannis.peloutier@ensea.fr>
2022-11-08 11:56:26 +0100

    AudioSignal copilot

--- src/main/java/audio/AudioSignal.java
+++ src/main/java/audio/AudioSignal.java
@@ -1,4 +1,70 @@
 package audio;
+import javax.sound.sampled.*;
+
+/** A container for an audio signal backed by a double buffer so as to allow floating point calculation
+ for signal processing and avoid saturation effects. Samples are 16 bit wide in this implementation. **/
 
 public class AudioSignal {
+
+    private double[] sampleBuffer;  // floating point representation of audio samples
+    private double dBLevel;         // current signal level
+    int frameSize;
+/**
+    public AudioSignal(FrameSize frameSize) {
+        sampleBuffer = new double[frameSize.getFrameSize()];
+    }
+**/
+    public void setFrom(AudioSignal other) {
+        System.arraycopy(other.sampleBuffer, 0, sampleBuffer, 0, sampleBuffer.length);
+    }
+
+    public boolean recordFrom(TargetDataLine audioInput) {
+        byte[] byteBuffer = new byte[sampleBuffer.length * 2]; // 16 bit samples
+        if (audioInput.read(byteBuffer, 0, byteBuffer.length) == -1) return false;
+           
+        for (int i = 0; i < sampleBuffer.length; i++) {
+                int sample = 0;
+                sample |= byteBuffer[2 * i] & 0xFF; // low byte
+                sample |= byteBuffer[2 * i + 1] << 8; // high byte
+                sampleBuffer[i] = sample / 32768.0; // signed 16 bit
+            }
+        // TODO : dbLevel = update signal level in dB here
+        return true; 
+    }
+
+    public boolean playTo(SourceDataLine audioOutput) {
+        byte[] byteBuffer = new byte[sampleBuffer.length * 2]; // 16 bit samples
+        for (int i = 0; i < sampleBuffer.length; i++) {
+            int sample = (int) (sampleBuffer[i] * 32768.0); // signed 16 bit
+            byteBuffer[2 * i] = (byte) (sample & 0xFF); // low byte
+            byteBuffer[2 * i + 1] = (byte) ((sample >> 8) & 0xFF); // high byte
+        }
+        if (audioOutput.write(byteBuffer, 0, byteBuffer.length) == -1) return false;
+        return true;
+    }
+
+    double getSample(int i) {
+        return sampleBuffer[i];
+    }
+
+    void setSample(int i, double value) {
+        sampleBuffer[i] = value;
+    }
+
+    double getdBLevel() {
+        return dBLevel;
+    }
+
+    int getFrameSize() {
+        return sampleBuffer.length;
+    }
+/**
+    Complex[] computeFFT() {
+        Complex[] complexSignal = new Complex[sampleBuffer.length];
+        for (int i = 0; i < sampleBuffer.length; i++) {
+            complexSignal[i] = new Complex(sampleBuffer[i], 0);
+        }
+        return FFT.fft(complexSignal);
+    }
+ **/
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.demo {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens ui to javafx.fxml;
