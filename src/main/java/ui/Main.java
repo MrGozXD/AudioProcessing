0a28431c4b5a7529f984c012ef04b6bd2577a8fc
MrGozXD <yannis.peloutier@ensea.fr>
2022-11-08 11:26:19 +0100

    ui+audio skeleton

--- src/main/java/ui/Main.java
+++ src/main/java/ui/Main.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml")); //variante du xml
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml")); //variante du xml
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);   //window dimension
         stage.setTitle("Hello Yannis!");   //window name
         stage.setScene(scene);
